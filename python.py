d6b5a32d079755042d32402c1591eeba55d55676
2kweef <toufeekpro@outlook.com>
2026-03-26 09:08:48 +0000

    added print

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
 numbers = []
 
-while len(numbers) < 10: 
+while len(numbers) < 10:
     number = int(input("Enter a number: "))
     if number not in numbers:
-    numbers.append(number)
+        numbers.append(number)
+print(numbers)
